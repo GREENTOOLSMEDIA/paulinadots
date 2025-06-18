808e511823d71384f489fd98106afe9d45acc00c
GREENTOOLSMEDIA <alarsi.atripper.agt@gmail.com>
2025-06-18 02:36:38 -0400

    Update include-footer.js

--- js/include-footer.js
+++ js/include-footer.js
@@ -1,1 +1,13 @@
+// js/include-footer.js
+fetch('footer.html')
+  .then(response => response.text())
+  .then(data => {
+    const container = document.getElementById('footer-container');
+    if (container) {
+      container.innerHTML = data;
+    } else {
+      console.error('No se encontró el contenedor #footer-container.');
+    }
+  })
+  .catch(error => console.error('Error al cargar el footer:', error));
 
